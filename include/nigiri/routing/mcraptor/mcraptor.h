0a45a21d7601fd55d283c654b38fe40bee59c494
Noah Langer <nlanger781@gmail.com>
2025-12-30 11:26:41 +0100

    update dest bag korrigiert

--- include/nigiri/routing/mcraptor/mcraptor.h
+++ include/nigiri/routing/mcraptor/mcraptor.h
@@ -676,7 +676,7 @@ private:
         tmp_label.success_chance = cum_enter_probability(i, k, tmp_label.arr_t_);
         dest_bag_.add(tmp_label, k);
       }
-      for (mcraptor_label tmp_label :tmp_[i].labels_) {
+      for (mcraptor_label tmp_label :location_bags_[i][k].labels_) {
         if(tmp_label.arr_t_ == kInvalid) continue;
         tmp_label.success_chance = cum_enter_probability(i, k, tmp_label.arr_t_);
         dest_bag_.add(tmp_label, k);
